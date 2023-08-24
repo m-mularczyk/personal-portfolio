849e44b92923476dfea212f47a2e506dc4b9d14d
Mular <mps.mooli@gmail.com>
2023-08-24 21:24:09 +0200

    Initial page structure and stylization prepared

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,13 @@
 console.log("Own JS Script loaded");
 
+let bannerButton = document.getElementsByClassName('button')[0].firstChild;
+bannerButton.onclick = changeButtonColor;
 
+function changeButtonColor(){
+  console.log("button clicked");
+  if(bannerButton.style.backgroundColor == "blue"){
+    bannerButton.style.backgroundColor = 'yellow';
+  } else {
+    bannerButton.style.backgroundColor = 'blue';
+  }
+};
